081343f2839552e451eea2b28bdbd53c55cfcb12
DAVIDLNAY <davidlenay@gmail.com>
2022-10-27 10:46:33 +0200

    commit 3

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
 const sidenav = document.querySelector("#mySidenav"); 
-      openBtn = document.querySelector("#openBtn");
-      closeBtn = document.querySelector("#closeBtn");
-      logo = document.querySelector(".logo-general");
-      articleLe = document.querySelector(".le");
-      ribbon = document.querySelector(".baniere-header");
-
-openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
-
+      openBtn = document.querySelector("#openBtn"); /*menu burger*/
+      closeBtn = document.querySelector("#closeBtn"); /*croix fermeture*/
+      logo = document.querySelector(".logo-general"); /*logo note de musique*/
+      articleLe = document.querySelector(".le"); /* LE */
+      
+if(openBtn && closeBtn) {
+  openBtn.onclick = openNav;
+  closeBtn.onclick = closeNav;
+}
 
 function openNav() {
   sidenav.classList.add("active");
   logo.style.zIndex = "5";
-  ribbon.style.zIndex = "5";
+  articleLe.style.zIndex = "1";
 }
 
 
 function closeNav() {
   sidenav.classList.remove("active");
   logo.style.zIndex = "1";
-  ribbon.style.zIndex = "1";
   articleLe.style.zIndex = "2";
 }
+////////////message alerte Nav2////////////
+
+
+const messageP2 = " Merci, Un professionnel disponible et compétent prendra contact avec toi par mail pour mettre en place des cours particulier."
+const btnSubmit = document.querySelector("#buttonProf");
+
+  btnSubmit?.addEventListener("click", function(event) {
+    console.log(event)
+    event.preventDefault();
+    alert(messageP2);
+  });
+
+  ////////////message alerte Nav1////////////
+
+  const messageNAv1 = "La recherche Map sera peut-être disponible dans une V2";
+  const buttonSearch = document.querySelector("#buttonSearch");
+
+  buttonSearch?.addEventListener("click", function(event) {
+    console.log(event)
+    event.preventDefault();
+    alert(messageNAv1);
+  });
+
+
+  ///////////////////////Function son checkbox//////////////////////
+
+
+  
+
+
+  
+
+  
+
+
+
+
+  
+
+
